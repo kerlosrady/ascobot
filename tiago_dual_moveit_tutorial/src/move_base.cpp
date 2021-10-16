c2b3f047280db57e466d5713a0266f5c36dbc850
kerols.rady <kerols.rady@gmail.com>
2021-10-16 06:56:54 +0200

    mkm

---
+++ tiago_dual_moveit_tutorial/src/move_base.cpp
@@ -0,0 +1,20 @@
+import rospy
+from geometry_msgs.msg import Twist
+import sys
+
+rospy.init_node("mobile_node")
+movement_publisher= rospy.Publisher('/mobile_base_controller/cmd_vel', Twist , queue_size=10)
+movement_cmd = Twist()
+movement_cmd.linear.x = 0.2
+movement_cmd.linear.y = 0
+movement_cmd.linear.z = 0
+movement_cmd.angular.x = 0
+movement_cmd.angular.y = 0              
+movement_cmd.angular.z = 0
+movement_publisher.publish(movement_cmd)
+
+
+
+
+rospy.spin()      
+ 
