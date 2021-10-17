9c899a894bc019c6ed42df1297828bad23838966
hendaafia1 <hend.aafia@gmail.com>
2021-10-17 09:55:47 +0200

    vision_v3

---
+++ vision/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/hendaafia2/ascobothub/vision/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/hendaafia2/anaconda3/condabin:/home/hendaafia2/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/hendaafia2/ascobothub/vision/build/catkin_tools_prebuild'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/hendaafia2/ascobothub/vision/devel/.private/catkin_tools_prebuild/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/hendaafia2/ascobothub/vision/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
---
+++ vision/devel/env.sh
@@ -0,0 +1,1 @@
+/home/hendaafia2/ascobothub/vision/devel/.private/catkin_tools_prebuild/env.sh
