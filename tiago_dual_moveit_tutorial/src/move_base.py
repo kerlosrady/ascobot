75eed9002f7a2f5b38fd6604b48f03b664155892
kerols.rady <kerols.rady@gmail.com>
2021-10-17 15:41:47 +0200

    k

--- tiago_dual_moveit_tutorial/src/move_base.py
+++ tiago_dual_moveit_tutorial/src/move_base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
 import sys
@@ -6,6 +6,7 @@ import sys
 def talker_ctrl():
     rospy.init_node("mobile_node")
     movement_publisher= rospy.Publisher('/mobile_base_controller/cmd_vel', Twist , queue_size=10)
+    rate = rospy.Rate(1) # 10hz
     movement_cmd = Twist()
     movement_cmd.linear.x = 0.2
     movement_cmd.linear.y = 0
@@ -14,7 +15,12 @@ def talker_ctrl():
     movement_cmd.angular.y = 0              
     movement_cmd.angular.z = 0
 
-while not rospy.is_shutdown():
-    movement_publisher.publish(movement_cmd)
-    rospy.spin()      
- 
+    while not rospy.is_shutdown():
+        movement_publisher.publish(movement_cmd)
+        rate.sleep()
+            
+if __name__=='__main__':
+     try:
+        talker_ctrl()
+     except rospy.ROSInterruptException:
+       pass
