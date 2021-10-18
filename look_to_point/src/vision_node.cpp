ec7033081dafa66db882c06778e1b474c9a4e9fc
hendaafia1 <hend.aafia@gmail.com>
2021-10-18 11:16:56 +0200

    vision_11

--- look_to_point/src/vision_node.cpp
+++ look_to_point/src/vision_node.cpp
@@ -69,9 +69,17 @@
 #include <ros/topic.h>
 
 // OpenCV headers
+
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <cv_bridge/cv_bridge.h>
+
+
+using namespace cv;
+using namespace std;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -83,9 +91,6 @@ static const std::string cameraInfoTopic = "/xtion/rgb/camera_info";
 
 // Intrinsic parameters of the camera
 cv::Mat cameraIntrinsics;
-cv::Mat image_with_circles;
-cv::Mat cannyOutput;
-cv::Mat medianImg;
 cv::Mat output;
 
 ros::Time latestImageStamp;
@@ -96,74 +101,36 @@ ros::Time latestImageStamp;
 void detectcircles (cv::Mat img)
 {
   //Covert to gray image
-  cv::cvtColor(img,img,cv::COLOR_BGR2GRAY);
-  cv::imshow(graywindowName, img);
+  cv::Mat grayImg
+  cv::cvtColor(img, grayImg, cv::COLOR_BGR2GRAY);
+  cv::imshow(graywindowName, grayImg);
 
   // //Apply Median Filter to eliminate noise 
-  cv::medianBlur(img,medianImg,5);
+  cv::Mat medianImg;
+  cv::medianBlur(grayImg,medianImg,5);
   cv::imshow("Median",medianImg);
 
   //Feature Detection
-   cv::Canny(medianImg,cannyOutput,70,210,5,0);
-   cv::imshow("Canny",cannyOutput);
+  cv::Mat cannyOutput;
+  cv::Canny(medianImg,cannyOutput,70,210,5,0);
+  cv::imshow("Canny",cannyOutput);
 
-  std::vector<std::vector<cv::Point> > contours;
-  std::vector<cv::Vec4i> hierarchy;
+  std::vector<Vec3f>  circles;
 
-
-  cv::findContours(cannyOutput,contours,hierarchy,cv::RETR_EXTERNAL,cv::CHAIN_APPROX_SIMPLE);
-
-  std::vector<cv::RotatedRect> minRect( contours.size() );
-
-  int counter = 1;
-  for( size_t i = 0; i< contours.size(); i++ )
+  cv::HoughCircles(medianImg, circles, cv::HOUGH_GRADIENT, 1, img.rows/64, 200, 10, 5, 30);
+  
+  for(size_t i=0; i<circles.size(); i++) 
   {
-      //Step 5
-      minRect[i] = cv::minAreaRect( contours[i] );
-      cv::Point2f rect_points[4];
-      minRect[i].points( rect_points );
 
-      //Step 6
-      if(contours[i].size()>50)
-      {
-          //Step 7
-          int centerX = (rect_points[0].x + rect_points[2].x)/2;
-          int centerY = (rect_points[0].y + rect_points[2].y)/2;
-          cv::Point2f a(centerX,centerY);
-
-          //Step 8 and Step 9
-          int sum = 0;
-          std::vector<int> storeLength;
-          for(int j=0; j<(int)contours[i].size(); j++)
-          {
-              cv::Point2f b(contours[i][j].x,contours[i][j].y);
-              int res = cv::norm(cv::Mat(a),cv::Mat(b));
-              sum += res;
-              storeLength.push_back(res);
-          }
-
-          int meanLength = sum / (int)storeLength.size();
-          int countBads = 0;
-          for(int u:storeLength)
-              if(abs(u-meanLength)>3)
-                  countBads++;
-
-          if(countBads<13)
-          {
-              for ( int j = 0; j < 13; j++ )
-              {
-                  line( output, rect_points[j], rect_points[(j+1)%4], cv::Scalar(0,0,255),3 );
-                  cv::putText(output,std::to_string(counter),cv::Point(centerX,centerY),cv::FONT_HERSHEY_SIMPLEX,1.0,cv::Scalar(0,255,255),3);
-              }
-              counter++;
-          }
-          else
-          {
-            output = cannyOutput;
-          }
-      }
-  }
-  cv::imshow("Output",output);
+    Vec3i c = circles[i];
+    Point center = Point(c[0], c[1]);
+    // circle center
+    circle( img, center, 1, Scalar(0,100,100), 3, LINE_AA);
+    // circle outline
+    int radius = cvRound(circles[i][2]);
+    circle( img, center, radius, Scalar(255,0,255), 3, LINE_AA);
+}
+    imshow("detected circles", img);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
