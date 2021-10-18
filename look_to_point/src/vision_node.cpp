4a4c2597c3ee1a5373f97c901e66cdaca90293d1
hendaafia1 <hend.aafia@gmail.com>
2021-10-18 14:49:34 +0200

    vision_25

--- look_to_point/src/vision_node.cpp
+++ look_to_point/src/vision_node.cpp
@@ -149,7 +149,7 @@ void detectcircles (cv::Mat img)
       int meanLength = sum / (int)storeLength.size();
       if(abs(meanLength)>10 && abs(meanLength)<50 )
       {
-        if (minRect[i] < 100) 
+        if ((abs(rect_points[0].x - rect_points[2].x)*abs(rect_points[0].y - rect_points[2].y)) < 100) 
         { if (abs(rect_points[0].x - rect_points[2].x) >20)
             { if (abs(rect_points[0].y - rect_points[2].y) >20) 
               { 
