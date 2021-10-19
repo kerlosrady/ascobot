88106b4f2d10472f9273efaa51bd6ea7af17b5b4
hendaafia1 <hend.aafia@gmail.com>
2021-10-19 13:50:08 +0200

    l

--- look_to_point/src/vision_node.cpp
+++ look_to_point/src/vision_node.cpp
@@ -141,7 +141,6 @@ void detectcircles (cv::Mat img)
       
       circle( img, a, 1, Scalar(0,100,100), 3, LINE_AA);
       int radius =  sqrt((rect_points[0].x - rect_points[2].x)*(rect_points[0].x - rect_points[2].x)+(rect_points[0].y - rect_points[2].y)*(rect_points[0].y - rect_points[2].y);
-      norm(rect_points[0],rect_points[2]);
       circle( img, a, radius, Scalar(255,0,255), 3, LINE_AA);
         
     }
