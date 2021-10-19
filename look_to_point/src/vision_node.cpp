8736be26a2251d1f1523fc4c78d99c60e73c66f0
hendaafia1 <hend.aafia@gmail.com>
2021-10-19 13:58:35 +0200

    l

--- look_to_point/src/vision_node.cpp
+++ look_to_point/src/vision_node.cpp
@@ -111,7 +111,7 @@ void detectcircles (cv::Mat img)
   cv::medianBlur(grayImg,medianImg,3);
 
   //Contour Detection
-  cv::Canny(medianImg,cannyOutput,80,100,1,0);
+  cv::Canny(medianImg,cannyOutput,80,100,3,0);
   cv::imshow("Canny",cannyOutput);
 
 
