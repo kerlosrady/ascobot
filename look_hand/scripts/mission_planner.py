ef7ba6b447f241ee1a1d4027ca9e8ac023bc8858
Verina <verina@LAPTOP-0CH026I8.localdomain>
2021-10-20 16:53:43 +0200

    ver

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from look_hand.srv import can_detection
-from look_hand.srv import grip
+
 #from look_hand.srv import shelf_detection
 
-state=1
+state=2
 
 
 class mission_planning():
@@ -17,18 +16,23 @@ class mission_planning():
 	#rospy.Subscriber("detection", object_pos, vision_callback)
 	#rospy.Subscriber("grip", bool, grip_callback)
 	rospy.Subscriber("control_arm",bool, control_arm_callback)
+	rospy.Subscriber("grip",bool, grip_callback)
+	rospy.Subscriber("can_detected",Float32MultiArray, can_detection_callback)
+
 
 	#rospy.Subscriber("control_base", base_states, control_base_callback)
-        self.pub = rospy.Publisher('arm_actions',Float32MultiArray, queue_size=10)
-        #self.pub2= rospy.Publisher('gripper', bool, queue_size=10)
+        self.pub = rospy.Publisher('arm_actions',g, queue_size=10)
+        self.pub2= rospy.Publisher('gripper', bool, queue_size=10)
 
 
-        #self.pub3= rospy.Publisher('camera_pos', int, queue_size=10)
+        self.pub3= rospy.Publisher('camera_pos', int, queue_size=10)
+	self.pub4= rospy.Publisher('can_detection', bool, queue_size=10)
+
 	
 
         rate = rospy.Rate(10) # 10hz
 	while not rospy.is_shutdown():
-	
+
 		# if state==0 and table_depth >= threshold:
 		#         #publish certain action to get back
 		#         state=1
@@ -37,62 +41,54 @@ class mission_planning():
 		#         pub1.publish(msgb)g
 
 		#if state==1 and table_depth - threshold >=0.1:
-		if state==1:
-			msgb = base_data()
-			msgb.stop =True
-			pub1.publish(msgb)
-			state =2
-			rospy.wait_for_service('can_detection')
-			try:
-				can_pos = rospy.ServiceProxy('can_detection', can_detection)
-				resp1 = can_detection(1)
-				x1, y1, z1 = resp1.can_loc1
-				x2, y2, z2 = resp1.can_loc2
-				cans_detected= True
-
-			except rospy.ServiceException as e:
-				print("Service call failed: %s" % e)
+		#if state==1:
+			#msgb = base_data()
+			#msgb.stop =True
+			#pub1.publish(msgb)
+			#state =2
+			
 
 		if state==2 and cans_detected is True:
 			state = 3
 			execute_state=1
 			cycle= cycle+1
 
-            		# if execute_state==1:
-		        while (done is not True):
+			# if execute_state==1:
+			while (done is not True):
 
 				if execute_state == 1:
 							
 					msg1 = Float32MultiArray
-				        msg1.loc1 = [x1, y1, z1, x2, y2, z2]
+					msg1= [x1, y1, z1, x2, y2, z2]
 					
-		                	pub.publish(msg1)
+					pub.publish(msg1)
 
 				if execute_state == 1 and reach_target == True:
-		                	execute_state = 2
-		                	reach_target= False
-					rospy.wait_for_service('can_detection')
+					execute_state = 2
+					reach_target= False
+					pub2.publish(True)
 
-					try:
-						can_pos = rospy.ServiceProxy('grip', grip)
-						resp1 = grip(1)
+					# rospy.wait_for_service('can_detection')
+
+					# try:
+					# 	can_pos = rospy.ServiceProxy('grip', grip)
+					# 	resp1 = grip(1)
 						
-						grip_target= resp1
+					# 	grip_target= resp1
 
 
-					except rospy.ServiceException as e:
-						print("Service call failed: %s" % e)
+					# except rospy.ServiceException as e:
+					# 	print("Service call failed: %s" % e)
 				
 
-		            	if execute_state == 3 and grip_target == True:
-		                
+				if execute_state == 2 and grip_target == True:
+						
 					z1= z1+0.1
 					z2= z2=0.1
-		                	msg1 = arm_actions()
-		                	msg1.loc1 = [x1, y1, z1]
-					msg1.loc1 = [x2, y2,z2]
-		                	execute_state = 4
-		                	grip_target= False
+					msg1 = Float32MultiArray
+					msg1 = [x1, y1, z1,x2,y2,z2]
+					execute_state = 3
+					grip_target= False
 					done= True
 
  
@@ -100,6 +96,26 @@ class mission_planning():
 
 		if data.data is True:
 			reach_target= True
+
+	def can_detection_callback(data):
+		can1_posx= data.data[0]
+		can1_posy= data.data[1]
+		can1_posz= data.data[2]
+
+		can2_posx= data.data[3]
+		can2_posy= data.data[4]
+		can2_posz= data.data[5]
+		cans_detected= True
+
+	def grip_callback(data):
+		if data.data is True:
+			grip_target= True
+
+
+
+
+
+	
 
 
 
