fe14ae32dee3ec4a83011be84a6aa3a62fe0c831
hendaafia1 <hend.aafia@gmail.com>
2021-10-20 18:45:31 +0200

    l

--- look_to_point/src/corner_detection.cpp
+++ look_to_point/src/corner_detection.cpp
@@ -116,7 +116,7 @@ void detecttable (cv::Mat img)
   Sobel(medianImg, sobelxy, CV_64F, 1, 1, 5);
 	imshow("Sobel XY using Sobel() function", sobelxy);
 
-  Canny(sobelxy, cannyOutput, 100, 200, 3, false);
+  Canny(sobelxy, cannyOutput, 100, 200, 3, 0);
 	imshow("Canny output", cannyOutput);
 }
 
