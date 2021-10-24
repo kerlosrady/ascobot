1a887766075423a15d2403cf415cdd58a3421082
Verina <verina@LAPTOP-0CH026I8.localdomain>
2021-10-24 16:01:05 +0200

    ve

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -3,7 +3,7 @@
 
 import rospy
 
-from std_msgs.msg import Int32
+from std_msgs.msg import Float32
 
 #from look_hand.srv import shelf_detection
 
@@ -17,18 +17,18 @@ rospy.init_node('mission_planner')
 
 def mission_planner():
 
-	rospy.Subscriber("control_arm", Int32, control_arm_callback)
-	rospy.Subscriber("grip",Int32, grip_callback)
+	rospy.Subscriber("control_arm", Float32, control_arm_callback)
+	rospy.Subscriber("grip",Float32, grip_callback)
 	rospy.Subscriber("can_detected",Float32MultiArray, can_detection_callback)
 
 
 	#rospy.Subscriber("control_base", base_states, control_base_callback)
         self.pub = rospy.Publisher('arm_actions',Float32MultiArray, queue_size=10)
-        self.pub2= rospy.Publisher('gripper', Int32, queue_size=10)
+        self.pub2= rospy.Publisher('gripper', Float32, queue_size=10)
 	self.pub3= rospy.Publisher('chatter_1',Float32, queue_size=10)
 
-        self.pub3= rospy.Publisher('camera_pos', Int32, queue_size=10)
-	self.pub4= rospy.Publisher('can_detection', Int32, queue_size=10)
+        self.pub3= rospy.Publisher('camera_pos', Float32, queue_size=10)
+	self.pub4= rospy.Publisher('can_detection', Float32, queue_size=10)
         rate = rospy.Rate(10) # 10hz
 
 
