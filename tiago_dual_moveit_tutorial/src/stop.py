73c74a4fba926df3107b59214ec5d8efce9347e2
kerlosrady <84878661+kerlosrady@users.noreply.github.com>
2021-10-24 07:42:36 +0200

    Update stop.py

--- tiago_dual_moveit_tutorial/src/stop.py
+++ tiago_dual_moveit_tutorial/src/stop.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Char
+from std_msgs.msg import Float32
 
 def firstNode(): 
-    pub = rospy.Publisher('chatter_1', Char, queue_size=10)
+    pub = rospy.Publisher('chatter_1', Float32, queue_size=10)
     rospy.init_node('firstNode', anonymous=True)
     rate = rospy.Rate(0.1) # 0.5Hz once every 2 seconds
 
     while not rospy.is_shutdown():
-        pub.publish('a')
+        pub.publish(5)
         rate.sleep()
                       
 if __name__=='__main__':
