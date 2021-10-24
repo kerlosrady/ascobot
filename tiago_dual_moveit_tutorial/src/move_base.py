aa045ee6355951f7d08860e49c4dcd32081d870a
kerlosrady <84878661+kerlosrady@users.noreply.github.com>
2021-10-24 06:08:10 +0200

    Update move_base.py

--- tiago_dual_moveit_tutorial/src/move_base.py
+++ tiago_dual_moveit_tutorial/src/move_base.py
@@ -6,18 +6,23 @@ import sys
 def talker_ctrl():
     rospy.init_node("mobile_node")
     movement_publisher= rospy.Publisher('/mobile_base_controller/cmd_vel', Twist , queue_size=10)
-    rate = rospy.Rate(1) # 10hz
+    rate = rospy.Rate(10) # 10hz
     movement_cmd = Twist()
-    movement_cmd.linear.x = 1.2
+    movement_cmd.linear.x = 5
     movement_cmd.linear.y = 0
     movement_cmd.linear.z = 0
     movement_cmd.angular.x = 0
     movement_cmd.angular.y = 0              
     movement_cmd.angular.z = 0
+    def stop_callback(event):
+        rospy.signal_shutdown("Just stopping publishing...")
+
+    rospy.Timer(rospy.Duration(3), stop_callback)
 
     while not rospy.is_shutdown():
         movement_publisher.publish(movement_cmd)
         rate.sleep()
+
             
 if __name__=='__main__':
      try:
