3103eec88eac5b6782f9129f37e1acc8c5b8629c
kerlosrady <84878661+kerlosrady@users.noreply.github.com>
2021-10-25 05:45:37 +0200

    Update stop.py

--- moveit_commander/script/stop.py
+++ moveit_commander/script/stop.py
@@ -5,12 +5,12 @@ from std_msgs.msg import Float32
 def firstNode(): 
     pub = rospy.Publisher('chatter_1', Float32, queue_size=10)
     rospy.init_node('firstNode', anonymous=True)
-    rate = rospy.Rate(0.5) # 0.1Hz once every 10 seconds
+    rate = rospy.Rate(0.1) # 0.1Hz once every 10 seconds
 
     while not rospy.is_shutdown():
         pub.publish(5)
-        pub.publish(6)
-        pub.publish(4)
+        #pub.publish(6)
+        #pub.publish(4)
         rate.sleep()
                       
 if __name__=='__main__':
