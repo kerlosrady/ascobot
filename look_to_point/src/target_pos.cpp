e8c89011d9c5b96b2fdb386c941c24b6523fc770
hendaafia1 <hend.aafia@gmail.com>
2021-10-25 15:34:08 +0200

    vision_can_v13

--- look_to_point/src/target_pos.cpp
+++ look_to_point/src/target_pos.cpp
@@ -170,9 +170,9 @@ class SubscribeAndPublish
       // //Apply Median Filter to eliminate noise 
       cv::medianBlur(grayImg,medianImg,11);
       cv::imshow("medianImg",medianImg);
-      cv::threshold(medianImg,medianImg,100,255,cv::THRESH_TOZERO_INV);
+      cv::threshold(medianImg,medianImg,120,255,cv::THRESH_TOZERO_INV);
       cv::imshow("threshold1",medianImg);
-      cv::threshold(medianImg,medianImg,60,255,cv::THRESH_TOZERO);
+      cv::threshold(medianImg,medianImg,90,255,cv::THRESH_TOZERO);
       cv::imshow("threshold2",medianImg);
       //Contour Detection
       cv::Canny(medianImg,cannyOutput,90,120,3,0);
