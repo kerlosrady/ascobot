238abf6b9cbd4798fcdc88cf24c14ea8dc1db3c4
Verina <verina@LAPTOP-0CH026I8.localdomain>
2021-10-25 11:50:00 +0200

    ver

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -20,106 +20,106 @@ state=1
 
 #class mission_planner():
 
-#def __init__(self):
+def __init__(self):
 
-def mission_planner():
-	rospy.Subscriber("control_arm", Float32, control_arm_callback)
-	rospy.Subscriber("grip",Float32, grip_callback)
-	rospy.Subscriber("can_detected",Float32MultiArray, can_detection_callback)
-
-
-	#rospy.Subscriber("control_base", base_states, control_base_callback)
-	pub = rospy.Publisher('arm_actions',Float32MultiArray, queue_size=10)
-	pub2= rospy.Publisher('gripper', Float32, queue_size=10)
-	pub3= rospy.Publisher('chatter_1',Float32, queue_size=10)
-
-	pub3= rospy.Publisher('camera_pos', Float32, queue_size=10)
-	pub4= rospy.Publisher('can_detection', Float32, queue_size=10)
-	rate = rospy.Rate(10) # 10hz
-	rospy.spin()
-
-	while not rospy.is_shutdown():
-
-		# if state==0 and table_depth >= threshold:
-		#         #publish certain action to get back
-		#         state=1
-		#         msgb= base_data()
-		#         msgb.loc= heading_pos
-		#         pub1.publish(msgb)g
-
-		#if state==1 and table_depth - threshold >=0.1:
-		if state==1:
-			#msgb = base_data()
-			#msgb.stop =True
-			#pub1.publish(msgb)
-			#state =2
-			pub3.publish(5)
-			
-
-		if state==2 and cans_detected is True:
-			state = 3
-			execute_state=1
-			cycle= cycle+1
-
-			# if execute_state==1:
-			while (done is not True):
-
-				if execute_state == 1:
-							
-					msg1 = Float32MultiArray
-					msg1= [x1, y1, z1, x2, y2, z2]
-					
-					pub.publish(msg1)
-
-				if execute_state == 1 and reach_target == True:
-					execute_state = 2
-					reach_target= False
-					pub2.publish(True)
-
-					# rospy.wait_for_service('can_detection')
-
-					# try:
-					# 	can_pos = rospy.ServiceProxy('grip', grip)
-					# 	resp1 = grip(1)
-						
-					# 	grip_target= resp1
+	def mission_planner(self):
+		self.rospy.Subscriber("control_arm", Float32, self.control_arm_callback)
+		self.rospy.Subscriber("grip",Float32, self.grip_callback)
+		self.rospy.Subscriber("can_detected",Float32MultiArray, self.can_detection_callback)
 
 
-					# except rospy.ServiceException as e:
-					# 	print("Service call failed: %s" % e)
+		#rospy.Subscriber("control_base", base_states, control_base_callback)
+		pub = rospy.Publisher('arm_actions',Float32MultiArray, queue_size=10)
+		pub2= rospy.Publisher('gripper', Float32, queue_size=10)
+		pub3= rospy.Publisher('chatter_1',Float32, queue_size=10)
+
+		pub3= rospy.Publisher('camera_pos', Float32, queue_size=10)
+		pub4= rospy.Publisher('can_detection', Float32, queue_size=10)
+		rate = rospy.Rate(10) # 10hz
+		rospy.spin()
+
+		while not rospy.is_shutdown():
+
+			# if state==0 and table_depth >= threshold:
+			#         #publish certain action to get back
+			#         state=1
+			#         msgb= base_data()
+			#         msgb.loc= heading_pos
+			#         pub1.publish(msgb)g
+
+			#if state==1 and table_depth - threshold >=0.1:
+			if state==1:
+				#msgb = base_data()
+				#msgb.stop =True
+				#pub1.publish(msgb)
+				#state =2
+				pub3.publish(5)
 				
 
-				if execute_state == 2 and grip_target == True:
+			if state==2 and cans_detected is True:
+				state = 3
+				execute_state=1
+				cycle= cycle+1
+
+				# if execute_state==1:
+				while (done is not True):
+
+					if execute_state == 1:
+								
+						msg1 = Float32MultiArray
+						msg1= [x1, y1, z1, x2, y2, z2]
 						
-					z1= z1+0.1
-					z2= z2=0.1
-					msg1 = Float32MultiArray
-					msg1 = [x1, y1, z1,x2,y2,z2]
-					execute_state = 3
-					grip_target= False
-					done= True
+						pub.publish(msg1)
+
+					if execute_state == 1 and reach_target == True:
+						execute_state = 2
+						reach_target= False
+						pub2.publish(True)
+
+						# rospy.wait_for_service('can_detection')
+
+						# try:
+						# 	can_pos = rospy.ServiceProxy('grip', grip)
+						# 	resp1 = grip(1)
+							
+						# 	grip_target= resp1
 
 
-def control_arm_callback(data):
+						# except rospy.ServiceException as e:
+						# 	print("Service call failed: %s" % e)
+					
 
-	if data.data is True:
-		reach_target= True
-
-def can_detection_callback(data):
-	can1_posx= data.data[0]
-	can1_posy= data.data[1]
-	can1_posz= data.data[2]
-
-	can2_posx= data.data[3]
-	can2_posy= data.data[4]
-	can2_posz= data.data[5]
-	cans_detected= True
-
+					if execute_state == 2 and grip_target == True:
+							
+						z1= z1+0.1
+						z2= z2=0.1
+						msg1 = Float32MultiArray
+						msg1 = [x1, y1, z1,x2,y2,z2]
+						execute_state = 3
+						grip_target= False
+						done= True
 
 
-def grip_callback(data):
-	if data.data is True:
-		grip_target= True
+	def control_arm_callback(self,data):
+
+		if data.data is True:
+			reach_target= True
+
+	def can_detection_callback(self,data):
+		can1_posx= data.data[0]
+		can1_posy= data.data[1]
+		can1_posz= data.data[2]
+
+		can2_posx= data.data[3]
+		can2_posy= data.data[4]
+		can2_posz= data.data[5]
+		cans_detected= True
+
+
+
+	def grip_callback(self,data):
+		if data.data is True:
+			grip_target= True
 
 
 if __name__=='__main__':
