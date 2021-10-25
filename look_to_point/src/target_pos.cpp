2c31ac554e3a6a0e2e5fa451e7df7e1ceb04ff07
hendaafia1 <hend.aafia@gmail.com>
2021-10-25 21:20:22 +0200

    l

--- look_to_point/src/target_pos.cpp
+++ look_to_point/src/target_pos.cpp
@@ -179,10 +179,9 @@ class SubscribeAndPublish
       grayTmpl= imread("/home/user/ws/src/ascobothub/look_to_point/src/tmp.png");
       cv::imshow("grayTmpl",grayTmpl);
       cv::Mat grayTmpl1;
-      // cv::resize(grayTmpl,grayTmpl1,Size(60, 70), INTER_LINEAR);
-      // cv::imshow("resize",grayTmpl1);
+      cv::resize(grayTmpl,grayTmpl1,Size(grayTmpl.rows, grayTmpl.cols), INTER_LINEAR);
+      cv::imshow("resize",grayTmpl1);
       cv::Mat grayTmpl2;
-      grayTmpl1.copyTo(grayTmpl2);
       cv::cvtColor(grayTmpl1, grayTmpl2, cv::COLOR_BGR2GRAY);
       cv::imshow("gray",grayTmpl2);
 
