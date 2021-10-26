c92ad8da380dc5e1ba2272b275007a09ca19bb65
Verina <verina@LAPTOP-0CH026I8.localdomain>
2021-10-26 20:30:41 +0200

    ver

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -2,27 +2,84 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-
-from std_msgs.msg import Float32,Float32MultiArray
-
+from std_msgs.msg import Float64,Float32MultiArray
 from nav_msgs.msg import Path
 from robot_helpers.robot_helpers import TransformServices
-
+from math import fabs
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 import numpy as np
+import tf2_ros
 import tf
-
-import geometry_msgs.msg
-
-#from look_hand.srv import shelf_detection
+from geometry_msgs.msg import PoseArray, PoseStamped, Pose, WrenchStamped, TransformStamped
+import geometry_msgs.msg 
 
 
 
-#rospy.init_node('mission_planner')
+class TransformServices():
+    def __init__(self):
+        self.transformer_listener = tf.TransformListener()
+        self.transformer_broadcaster = tf2_ros.StaticTransformBroadcaster()
+
+    def transform_poses(self, target_frame, source_frame, pose_arr):
+        """
+        Transform poses from source_frame to target_frame
+        """
+        trans_pose_arr = PoseArray()
+        for i in range(len(pose_arr.poses)):
+            trans_pose = PoseStamped()
+            pose = PoseStamped()
+            pose.header.frame_id = source_frame
+            pose.pose = pose_arr.poses[i]
+            self.transformer_listener.waitForTransform(
+                target_frame, source_frame, rospy.Time(), rospy.Duration(1))
+            trans_pose = self.transformer_listener.transformPose(
+                target_frame, pose)
+            trans_pose_arr.poses.append(trans_pose.pose)
+
+        trans_pose_arr.header.frame_id = target_frame
+        trans_pose_arr.header.stamp = rospy.Time()
+        return trans_pose_arr
+
+    def lookup_transform(self, target_frame, source_frame):
+        self.transformer_listener.waitForTransform(
+            target_frame, source_frame, rospy.Time(), rospy.Duration(1))
+        t, r = self.transformer_listener.lookupTransform(
+            target_frame, source_frame, rospy.Time())
+        pose = Pose()
+        pose.position.x = t[0]
+        pose.position.y = t[1]
+        pose.position.z = t[2]
+        pose.orientation.x = r[0]
+        pose.orientation.y = r[1]
+        pose.orientation.z = r[2]
+        pose.orientation.w = r[3]
+        return pose
+
+    def create_frame(self, ref_frame, moving_frame, new_frame):
+        pose = self.lookup_transform(ref_frame, moving_frame)
+        return self.create_frame_at_pose(pose, ref_frame, new_frame)
+    
+    def create_frame_at_pose(self, pose, ref_frame, new_frame):
+        translation = [pose.position.x, pose.position.y, pose.position.z]
+        orientation = [pose.orientation.x, pose.orientation.y,
+                       pose.orientation.z, pose.orientation.w]
+        static_transformStamped = TransformStamped()
+        static_transformStamped.header.stamp = rospy.Time.now()
+        static_transformStamped.header.frame_id = ref_frame
+        static_transformStamped.child_frame_id = new_frame
+
+        static_transformStamped.transform.translation.x = translation[0]
+        static_transformStamped.transform.translation.y = translation[1]
+        static_transformStamped.transform.translation.z = translation[2]
+
+        static_transformStamped.transform.rotation.x = orientation[0]
+        static_transformStamped.transform.rotation.y = orientation[1]
+        static_transformStamped.transform.rotation.z = orientation[2]
+        static_transformStamped.transform.rotation.w = orientation[3]
+        self.transformer_broadcaster.sendTransform(static_transformStamped)
+        return pose
 
 
-#rospy.init_node('mission_planning')
-#rospy.Subscriber("detection", object_pos, vision_callback)
-#rospy.Subscriber("grip", bool, grip_callback)
 
 
 
@@ -215,32 +272,48 @@ class mission_planner():
 			self.msgcamera_id= data.header.frame_id
 			self.msgcamera_poses =data.poses
 			
-			num_cans= len(self.msgcamera_poses),3
+			num_cans= len(self.msgcamera_poses)
 			campos= np.empty(num_cans,3)
 			for i in range(len(num_cans)):
 				tempar= np.empty(3)
 				tempar[0]= self.msgcamera_poses[i].pose.position.x
 				tempar[1]= self.msgcamera_poses[i].pose.position.y
 				tempar[2]= self.msgcamera_poses[i].pose.position.z
-				
-				
 				campos[i]=tempar
 
 			col_y=campos[campos[:,1].argsort()]
 			
-			#col_x = col_y[col_y[:,0].argsort()]
-			
-			if totalCans%4== 0:
+			if num_cans%4== 0:
 
-				first_row= col_y[:3]
+				first_row= col_y[-4:-1]
 				col_x = first_row[first_row[:,0].argsort()]
-				selectedCans =col_x
+				selectedCans =np.empty(2,3)
+				selectedCans[0]= col_x[0]
+				selectedCans[1]= col_x[-1]
+
 			else:
 				first_row= col_y[:1]
 				selectedCans= first_row
+				
+			tfs= PoseArray()
+			tfs.header.frame_id= self.msgcamera_id
+
+			tfsp1 = PoseStamped()
+			tfsp1.header.frame_id = self.msgcamera_id
+			tfsp1.pose.position.x= selectedCans[0][0]
+			tfsp1.pose.position.y= selectedCans[0][1]
+			tfsp1.pose.position.z= selectedCans[0][2]
+			tfs.poses.append(tfsp1)
+
+			tfsp2 = PoseStamped()
+			tfsp2.header.frame_id = self.msgcamera_id
+			tfsp2.pose.position.x= selectedCans[0][0]
+			tfsp2.pose.position.y= selectedCans[0][1]
+			tfsp2.pose.position.z= selectedCans[0][2]
+			tfs.poses.append(tfsp2)
 			
-			
-			
+			Trans=TransformServices()
+			Trans.transform_poses(self.msgcamera_id,'/base_link',tfs)
 			print(selectedCans)
 			
 			print(data.poses)
