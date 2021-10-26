5bffb2ead50c5e904f6adf4782cec59855ef3b24
Verina <verina@LAPTOP-0CH026I8.localdomain>
2021-10-26 21:53:33 +0200

    ver

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -242,14 +242,14 @@ class mission_planner():
 			print(num_cans)
 			col_y=campos[i in campos[:,1].argsort()]
 			print("col_y",col_y)
-			ccc = col_y[0]
-			print("ccc", ccc)
-
+			col_y = col_y[0]
 			
 			if num_cans%4== 0:
 
 				first_row= col_y[-4:-1]
+				print("1st row", first_row)
 				col_x = first_row[i in first_row[:,0].argsort()]
+				col_x = col_x(0)
 				print("colx",col_x)
 				selectedCans =np.ones((2,3))
 				selectedCans[0]= col_x[0]
