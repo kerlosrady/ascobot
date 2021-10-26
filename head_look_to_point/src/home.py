8443be2384a2539dac70e73fcfb5238355c89e76
Reemgh <reemhesham1999@gmail.com>
2021-10-26 17:53:17 +0200

    reem

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -16,10 +16,12 @@ def callback(msg):
     for I in range(0,360):
         print (I , msg.ranges[I]) # the center value of the ranges' array , assuming the range is 720 (no.of array elements i.e. laser beams)
         if msg.ranges[I] < 0.8 :
-           break
+	    pub.publish("S")
+        else:
+            pub.publish("M")
          #any(msg.ranges[100:360]<0.5): #when the center distance to the obstacle becomes less than 0.5 the robot should stop
 		
-	pub.publish(move)
+
 	
 rospy.init_node('check_obstacle') # Initializes a node      
     
@@ -27,11 +29,9 @@ sub = rospy.Subscriber("/scan", LaserScan, callback)  # Subscriber object which 
                                                       # from the "/scan" Topic and call the "callback" function
 						      # each time it reads something from the Topic
 
-pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)  # Publisher object which will publish "Twist" type messages
+pub = rospy.Publisher("/dis_action", String::std, queue_size=10)  # Publisher object which will publish "Twist" type messages
                             				 # on the "/cmd_vel" Topic, "queue_size" is the size of the
                                                          # outgoing message queue used for asynchronous publishing
-
-move = Twist() # Creates a Twist message type object
 
 
 rospy.spin() # Loops infinitely until someone stops the program execution
