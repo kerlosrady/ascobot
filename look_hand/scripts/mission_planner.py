04b1a7beb0d1c63b956480f127495809919ee86d
Verina <verina@LAPTOP-0CH026I8.localdomain>
2021-10-26 14:22:27 +0200

    ver

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -7,7 +7,7 @@ from std_msgs.msg import Float32,Float32MultiArray
 
 from nav_msgs.msg import Path
 
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PointStamped
 
 
 import tf
