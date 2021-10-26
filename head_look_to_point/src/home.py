56a053de6259dbf9da8aa8f173d4100804f4ae5b
Reemgh <reemhesham1999@gmail.com>
2021-10-26 17:28:41 +0200

    reem

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -15,7 +15,7 @@ def callback(msg):
 
     for I in range(0,360):
         print (I , msg.ranges[I]) # the center value of the ranges' array , assuming the range is 720 (no.of array elements i.e. laser beams)
-        if msg.ranges[I] < 100 :
+        if msg.ranges[I] < 0.8 :
            break
          #any(msg.ranges[100:360]<0.5): #when the center distance to the obstacle becomes less than 0.5 the robot should stop
 		
