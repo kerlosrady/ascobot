0073a691c62eff0061bb9fc30128148ff8adba16
kerlosrady <84878661+kerlosrady@users.noreply.github.com>
2021-10-26 14:30:39 +0200

    Update pub.py

--- moveit_commander/script/pub.py
+++ moveit_commander/script/pub.py
@@ -7,12 +7,12 @@ from std_msgs.msg import Float32
 rospy.init_node("publisher_node")
 
 # create publisher
-publisher = rospy.Publisher('rarm', Float32MultiArray, queue_size = 10)
+publisher = rospy.Publisher('larm', Float32MultiArray, queue_size = 10)
 publisher2 = rospy.Publisher('gripper', Float32, queue_size = 10)
 
 while not rospy.is_shutdown():
 	msg = Float32MultiArray()
-	msg.data = [0.6937500,-0.1376100,0.7151]
+	msg.data = [0.064765,0.83785,0.7151]
         #publisher2.publish(11)
 	publisher.publish(msg)
 	rospy.sleep(1)
