c0e1e93cd327866081f61ac1cafe3bd29cea13fb
Reemgh <reemhesham1999@gmail.com>
2021-10-26 17:26:50 +0200

    reem

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -15,8 +15,8 @@ def callback(msg):
 
     for I in range(0,360):
         print (I , msg.ranges[I]) # the center value of the ranges' array , assuming the range is 720 (no.of array elements i.e. laser beams)
-        #if msg.ranges[I] < 100 :
-         #   break
+        if msg.ranges[I] < 100 :
+           break
          #any(msg.ranges[100:360]<0.5): #when the center distance to the obstacle becomes less than 0.5 the robot should stop
 		
 	pub.publish(move)
