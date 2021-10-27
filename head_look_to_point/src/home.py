3380f2e8569c6ec8de91d37cdbd4e47d07ae2ff0
hendaafia1 <hend.aafia@gmail.com>
2021-10-27 14:41:49 +0200

    hend

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -15,9 +15,9 @@ from std_msgs.msg import Float32
 def callback(msg):
 
     for I in range(0,360):
-        print (I , msg.ranges[I]) # the center value of the ranges' array , assuming the range is 720 (no.of array elements i.e. laser beams)
         if msg.ranges[I] < 0.8 :
-	        pub2.publish(999)
+            print("You should Stop")
+            pub2.publish(999)
         else:
             pass
          #any(msg.ranges[100:360]<0.5): #when the center distance to the obstacle becomes less than 0.5 the robot should stop
