d008c48f495bfa4a705a30a27f710b055c08a550
kerlosrady <84878661+kerlosrady@users.noreply.github.com>
2021-10-27 23:58:25 +0200

    Update move_base.py

--- moveit_commander/script/move_base.py
+++ moveit_commander/script/move_base.py
@@ -5,6 +5,7 @@ from std_msgs.msg import Float32
 from std_msgs.msg import String
 import sys
 
+
 rospy.init_node("mobile_node")
 movement_publisher= rospy.Publisher('/mobile_base_controller/cmd_vel', Twist , queue_size=10)
 stop_publisher= rospy.Publisher('base_state', String , queue_size=10)
@@ -12,58 +13,75 @@ stop_publisher= rospy.Publisher('base_state', String , queue_size=10)
 def callback(data):
     if data.data==5:
     	for_ctrl()
-
     if data.data==4:
-    	rot_ctrl()
-    	
-def callback2(data):
-    if data.data==999:
-    	stop_publisher.publish("arrived") 
-    	stop_callback()
-    if data.data==666:
-    	stop_publisher.publish("rotated") 
-    	stop_callback()
-    	
+    	global vx
+        vx.data=2
+    for_ctrl()
+
+vx=Float32()
+vx.data=0
+
+movement_cmd2 = Twist()  
+movement_cmd2.linear.x = 0
+movement_cmd2.linear.y = 0
+movement_cmd2.linear.z = 0
+movement_cmd2.angular.x = 0
+movement_cmd2.angular.y = 0              
+movement_cmd2.angular.z = 0
+
 def for_ctrl():
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(3) # 10hz
     movement_cmd = Twist()
-    movement_cmd.linear.x = 0.8
+    
+    movement_cmd.linear.x = 2
     movement_cmd.linear.y = 0
     movement_cmd.linear.z = 0
     movement_cmd.angular.x = 0
-    movement_cmd.angular.y = 0              
-    movement_cmd.angular.z = 0
-
+    movement_cmd.angular.y = 0
+    movement_cmd.angular.z = -0.07
+    
+    def stop_callback(data):
+        if data.data==15.0:
+            print("kkk")
+            global vx
+            vx.data=1
+            return
+        if data.data==666:
+            global vx
+            vx.data=3
+            return
+    rospy.Subscriber("lidar_reading", Float32, stop_callback)
+           
     while not rospy.is_shutdown():
-        movement_publisher.publish(movement_cmd)
-        rate.sleep()
-        
-def stop_callback():
-        rospy.signal_shutdown("Just stopping publishing...")
-        rospy.init_node("mobile_node")
-
-        
-def rot_ctrl():
-    rate = rospy.Rate(10) # 10hz
-    movement_cmd = Twist()
-    movement_cmd.linear.x = 0
-    movement_cmd.linear.y = 0
-    movement_cmd.linear.z = 0
-    movement_cmd.angular.x = 0
-    movement_cmd.angular.y = 0              
-    movement_cmd.angular.z = 0.26
-    while not rospy.is_shutdown():
-        movement_publisher.publish(movement_cmd)
+        if vx.data==0:
+            movement_publisher.publish(movement_cmd)
+        if vx.data==1:
+            movement_publisher.publish(movement_cmd2)
+            stop_publisher.publish("arrived")
+            baseNode()
+        if vx.data==2:
+            movement_publisher.publish(movement_cmd3)
+            stop_publisher.publish("rotating") 
+        if vx.data==3:
+            movement_publisher.publish(movement_cmd2)
+            stop_publisher.publish("rotated")
         rate.sleep()
 
+movement_cmd3 = Twist()
+movement_cmd3.linear.x = 0
+movement_cmd3.linear.y = 0
+movement_cmd3.linear.z = 0
+movement_cmd3.angular.x = 0
+movement_cmd3.angular.y = 0              
+movement_cmd3.angular.z = 0.3
+    
 def baseNode():
       rospy.Subscriber('chatter_1', Float32, callback)
-      rospy.Subscriber('Lidar_reading', Float32, callback2)
+      print
       rospy.spin()
             
 if __name__=='__main__':
      try:
-
         baseNode()
      except rospy.ROSInterruptException:
        pass
