876ec0910fb9097c99ad75d99aaa5e5ad4f7ea44
kerlosrady <84878661+kerlosrady@users.noreply.github.com>
2021-10-27 14:15:13 +0200

    Update home.py

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -5,6 +5,7 @@ import rospy # Python library for ROS
 from sensor_msgs.msg import LaserScan # LaserScan type message is defined in sensor_msgs
 from geometry_msgs.msg import Twist 
 
+
 #ranges array contains alot of messages, each message is a laser beam 
 #laser beams cover 180 deg
 #ranges are inf(no obstacles on the right side),values(at the center)(represent center distance to an obstacle right in front of the robot),inf(no obstacles on the left side)
@@ -16,9 +17,9 @@ def callback(msg):
     for I in range(0,360):
         print (I , msg.ranges[I]) # the center value of the ranges' array , assuming the range is 720 (no.of array elements i.e. laser beams)
         if msg.ranges[I] < 0.8 :
-	    pub.publish("S")
+	    pub2.publish(999)
         else:
-            pub.publish("M")
+            pub2.publish("M")
          #any(msg.ranges[100:360]<0.5): #when the center distance to the obstacle becomes less than 0.5 the robot should stop
 		
 
@@ -29,10 +30,10 @@ sub = rospy.Subscriber("/scan", LaserScan, callback)  # Subscriber object which 
                                                       # from the "/scan" Topic and call the "callback" function
 						      # each time it reads something from the Topic
 
-pub = rospy.Publisher("/dis_action", String::std, queue_size=10)  # Publisher object which will publish "Twist" type messages
+#pub = rospy.Publisher("/dis_action", String::std, queue_size=10)  # Publisher object which will publish "Twist" type messages
                             				 # on the "/cmd_vel" Topic, "queue_size" is the size of the
                                                          # outgoing message queue used for asynchronous publishing
-
+pub2=rospy.Publisher("Lidar_reading", Float32::std, queue_size=10)
 
 rospy.spin() # Loops infinitely until someone stops the program execution
 
