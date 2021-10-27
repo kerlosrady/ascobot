f0a3e520c666229db3bf86aa2a169b316213bd53
tony2500 <92026889+tony2500@users.noreply.github.com>
2021-10-27 23:21:41 +0200

    Update home.py

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -9,8 +9,6 @@ from std_msgs.msg import Float32
 def callback(msg):
 
     for I in range(0,360):
-	if I == 180 :
-	    print(msg.ranges[I])
         if msg.ranges[I] < 0.8 :
             print("You should Stop")
             pub2.publish(999)
@@ -23,7 +21,5 @@ sub = rospy.Subscriber("/scan", LaserScan, callback)
 print(type(sub))
 # outgoing message queue used for asynchronous publishing
 pub2 = rospy.Publisher("/lidar_reading", Float32, queue_size=10)
-for I in range(0,361):
-	if I == 360 :
-	    print(sub.ranges[I])
+
 rospy.spin() # Loops infinitely until someone stops the program execution
