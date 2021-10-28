3b726ac50547c2fcfeab7d1a945d876a72c83815
hendaafia1 <hend.aafia@gmail.com>
2021-10-28 14:26:31 +0200

    hend

--- look_to_point/src/can_dtct.cpp
+++ look_to_point/src/can_dtct.cpp
@@ -79,22 +79,6 @@ class SubscribeAndPublish
     {
       while (ros::ok())
       {
-
-        // Get the camera intrinsic parameters from the appropriate ROS topic
-        ROS_INFO("Waiting for camera intrinsics ... ");
-        sensor_msgs::CameraInfoConstPtr msg = ros::topic::waitForMessage
-            <sensor_msgs::CameraInfo>(cameraInfoTopic, ros::Duration(10.0));
-
-        if(msg.use_count() > 0)
-        {
-          cameraIntrinsics = cv::Mat::zeros(3,3,CV_64F);
-          cameraIntrinsics.at<double>(0, 0) = msg->K[0]; //fx
-          cameraIntrinsics.at<double>(1, 1) = msg->K[4]; //fy
-          cameraIntrinsics.at<double>(0, 2) = msg->K[2]; //cx
-          cameraIntrinsics.at<double>(1, 2) = msg->K[5]; //cy
-          cameraIntrinsics.at<double>(2, 2) = 1;
-        }
-        
         // Define ROS topic from where TIAGo publishes images
         // use compressed image transport to use less network bandwidth
         ROS_INFO_STREAM("Subscribing ");
@@ -242,7 +226,20 @@ int main(int argc, char** argv)
 {
   // Initialize the ROS node
   ros::init(argc, argv, "Vision");
+  // Get the camera intrinsic parameters from the appropriate ROS topic
+  ROS_INFO("Waiting for camera intrinsics ... ");
+  sensor_msgs::CameraInfoConstPtr msg = ros::topic::waitForMessage
+      <sensor_msgs::CameraInfo>(cameraInfoTopic, ros::Duration(10.0));
 
+  if(msg.use_count() > 0)
+  {
+    cameraIntrinsics = cv::Mat::zeros(3,3,CV_64F);
+    cameraIntrinsics.at<double>(0, 0) = msg->K[0]; //fx
+    cameraIntrinsics.at<double>(1, 1) = msg->K[4]; //fy
+    cameraIntrinsics.at<double>(0, 2) = msg->K[2]; //cx
+    cameraIntrinsics.at<double>(1, 2) = msg->K[5]; //cy
+    cameraIntrinsics.at<double>(2, 2) = 1;
+  }
   ROS_INFO("Starting Vision application ...");
   
   SubscribeAndPublish SAPObject;
