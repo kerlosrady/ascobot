3eabf058c02614e7ae139d0af8b98a212192a25c
hendaafia1 <hend.aafia@gmail.com>
2021-10-28 11:07:48 +0200

    p

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -157,7 +157,7 @@ class mission_planner():
 						apose_goal1 = np.ones(7)
 						apose_goal1[0]=self.finalPoints_r.poses[0].position.x
 						apose_goal1[1]=self.finalPoints_r.poses[0].position.y
-						apose_goal1[2]=self.finalPoints_r.poses[0].position.z+0.3
+						apose_goal1[2]=self.finalPoints_r.poses[0].position.z+0.5
 						
 						apose_goal1[3]=0
 						apose_goal1[4]=0
@@ -171,7 +171,7 @@ class mission_planner():
 						apose_goal2 = np.ones(7)
 						apose_goal2[0]=self.finalPoints_l.poses[1].position.x
 						apose_goal2[1]=self.finalPoints_l.poses[1].position.y
-						apose_goal2[2]=self.finalPoints_l.poses[1].position.z+0.3
+						apose_goal2[2]=self.finalPoints_l.poses[1].position.z+0.5
 						
 						apose_goal2[3]=0
 						apose_goal2[4]=0
