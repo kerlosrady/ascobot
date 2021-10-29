c65d03477e61470721c6c9d9a6253f4ce454134f
Verina <verina@LAPTOP-0CH026I8.localdomain>
2021-10-29 20:41:07 +0200

    ver

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -181,18 +181,15 @@ class mission_planner():
 						pose_goal2= Float32MultiArray(data =apose_goal2 )
 						rospy.sleep(1)						
 						
-
-
-						self.pubr.publish(pose_goal1)
-						print("done publishing goal 1")
-						print(pose_goal1)
-
 						self.publ.publish(pose_goal2)
 						print("done publishing goal 2")
 						print(pose_goal2)
 						self.execute_state = 2
 
-						
+						self.pubr.publish(pose_goal1)
+						print("done publishing goal 1")
+						print(pose_goal1)
+
 
 					if self.execute_state == 2 and self.LarmReach==True and self.RarmReach==True:
 						self.execute_state = 3
