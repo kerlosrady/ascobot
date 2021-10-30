64f24cfbde12ceac1e1917cf04f37f30b0d6ecdd
Verina <verina@LAPTOP-0CH026I8.localdomain>
2021-10-30 15:12:45 +0200

    ver

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -213,9 +213,11 @@ class mission_planner():
 
 					#if self.execute_state==4 and self.RarmReach==True:
 					if self.execute_state==4:
+						rospy.sleep(1)
 						while self.BrotateState==False:
 						
 							self.pub4.publish(4.0)
+							print("Rotating")
 						
 						
 						#self.RarmReach= False
