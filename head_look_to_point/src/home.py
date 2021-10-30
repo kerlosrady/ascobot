2dfd0001589ad878c667d5ed2c2829fe70dc74ef
kerlosrady <84878661+kerlosrady@users.noreply.github.com>
2021-10-30 18:58:35 +0200

    Update home.py

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -22,7 +22,7 @@ def callback(msg):
         else:
             pass
 	# print(msg.ranges[360])
-        if msg.ranges[360] < 0.9 :
+        if msg.ranges[360] < 0.85 :
             print("You should Stop")
 	    pub2.publish(15.0)
             if n.dat<3:
