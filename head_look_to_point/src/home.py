dba5e0507d887d52ff8b49496cf8a360993a37a1
hendaafia1 <hend.aafia@gmail.com>
2021-10-31 19:43:37 +0200

    r

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -10,10 +10,6 @@ n=Float32()
 n.data=0
 def callback(msg):
     global n
-    x = min(msg.ranges)
-    #print(msg.ranges.index(x))
-    #ind = msg.ranges.where(msg.ranges[:]==x)
-    print(msg.ranges.index(x))
     for I in range(0,360):
         if msg.ranges[I] < 1.3 :
             # print("You should Stop")
@@ -24,26 +20,22 @@ def callback(msg):
             return
         else:
             pass
-<<<<<<< HEAD
 
 	print(msg.ranges[333])
 
-=======
-	#print(msg.ranges[333])
->>>>>>> e578cbd530d50ca01050d4117542dc82e9b30f79
         if msg.ranges[360] < 0.4:
             #print("You should Stop")
             if n.data<5:
                 pub2.publish(666)
                 n.data=n.data+1	
-                return
+            return
        
         if msg.ranges[360] > 0.7 and msg.ranges[360] < 0.72:
             #print("You should Stop")
             if n.data<8:
                 pub2.publish(66666)
                 n.data=n.data+1	
-	            return
+            return
         
 rospy.init_node('check_obstacle') # Initializes a node      
 # Subscriber object which will listen "LaserScan" type messages
