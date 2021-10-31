68cf2645a3d75670b41b5c3248d40a35456e1b8f
tony2500 <92026889+tony2500@users.noreply.github.com>
2021-10-31 21:01:14 +0200

    Update home.py

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -11,6 +11,7 @@ n.data=0
 def callback(msg):
     global n
     for I in range(0,360):
+		print(msg.ranges[I])
         if msg.ranges[I] < 1.3 :
             # print("You should Stop")
             if n.data==0:
