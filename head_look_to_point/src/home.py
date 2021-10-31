bfa586edf20ccd4f4bd6bf4fbe46c65353a0c37e
hendaafia1 <hend.aafia@gmail.com>
2021-10-31 21:13:26 +0200

    r

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -10,10 +10,12 @@ n=Float32()
 n.data=0
 
 first_time = 0
-
+done_rotating = 0 
 def callback(msg):
     global n
     global first_time
+    global done_rotating
+
     for I in range(0,360):
 		# print(msg.ranges[I])
         if first_time == 0:
@@ -35,9 +37,10 @@ def callback(msg):
             if n.data<5:
                 pub2.publish(666)
                 n.data=n.data+1	
+                done_rotating =1 
             return
         
-        if msg.ranges[360] > 0.45 and msg.ranges[360] < 0.47:
+        if msg.ranges[360] > 0.45 and msg.ranges[360] < 0.47 and done_rotating == 1:
             print("You should Stop bcuz of multi")
             if n.data<8:
                 pub2.publish(66666)
