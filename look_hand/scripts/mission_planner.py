322b887fbf3b02b23aea629c9d89612e3f954fd8
hendaafia1 <hend.aafia@gmail.com>
2021-10-31 17:02:07 +0200

    hend

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -263,8 +263,7 @@ class mission_planner():
 						apose_goal1 = np.ones(4)
 						apose_goal1[0]=self.finalPoints.poses[0].position.x-0.27
 						apose_goal1[1]=self.finalPoints.poses[0].position.y + 0.03
-						apose_goal1[2]=self.finalPoints.poses[0].position.z
-						
+						apose_goal1[2]=self.finalPoints.poses[0].position.z +0.05
 						apose_goal1[3]=2
 						
 						pose_goal1= Float32MultiArray(data =apose_goal1 )
@@ -275,12 +274,8 @@ class mission_planner():
 						apose_goal2 = np.ones(7)
 						apose_goal2[0]=self.finalPoints.poses[1].position.x-0.27
 						apose_goal2[1]=self.finalPoints.poses[1].position.y + 0.03
-						apose_goal2[2]=self.finalPoints.poses[1].position.z 
-						
-						apose_goal2[3]=self.finalPoints.poses[1].orientation.x
-						apose_goal2[4]=self.finalPoints.poses[1].orientation.y
-						apose_goal2[5]=self.finalPoints.poses[1].orientation.z
-						apose_goal2[6]=self.finalPoints.poses[1].orientation.w
+						apose_goal2[2]=self.finalPoints.poses[1].position.z +0.05
+						apose_goal2[3]=2
 						pose_goal2= Float32MultiArray(data =apose_goal2 )
 						rospy.sleep(1)	
 					
