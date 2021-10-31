e76ae5b9fedd5c333d71e4e10cc3beeeec7a5754
hendaafia1 <hend.aafia@gmail.com>
2021-10-31 14:41:13 +0200

    hend

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -22,7 +22,7 @@ def callback(msg):
         else:
             pass
 	#print(msg.ranges[360])
-        if msg.ranges[360] < 0.4 :
+        if msg.ranges[360] < 0.8 :
             #print("You should Stop")
             pub2.publish(15.0)
             if n.data<3:
