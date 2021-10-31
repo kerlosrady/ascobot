6e5d225a819817e6cede4b7f87fcd590ccfdb5b2
tony2500 <92026889+tony2500@users.noreply.github.com>
2021-10-31 21:37:37 +0200

    Update home.py

--- head_look_to_point/src/home.py
+++ head_look_to_point/src/home.py
@@ -40,7 +40,7 @@ def callback(msg):
                 done_rotating =1 
             return
         
-        if msg.ranges[360] > 0.45 and msg.ranges[360] < 0.47 and done_rotating == 1:
+        if msg.ranges[360] > 0.3 and msg.ranges[360] < 0.3 and done_rotating == 1:
             print("You should Stop bcuz of multi")
             if n.data<8:
                 pub2.publish(66666)
