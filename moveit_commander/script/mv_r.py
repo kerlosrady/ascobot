0b2c21d18dca5f05685b33805fb87d19dc9dc349
hendaafia1 <hend.aafia@gmail.com>
2021-11-01 07:43:00 +0200

    hend

--- moveit_commander/script/mv_r.py
+++ moveit_commander/script/mv_r.py
@@ -272,7 +272,7 @@ def callback2(data):
   pub3 = rospy.Publisher('confirmation_gr', String, queue_size=10)
   if data.data==11:
     tutorial = MoveGroupPythonInterfaceTutorial()
-    tutorial.rgrip_pose_goal(0.032,0.032) #gripped
+    tutorial.rgrip_pose_goal(0.033,0.033) #gripped
     pub3.publish("gripped")
   if data.data==0:
     tutorial = MoveGroupPythonInterfaceTutorial()
