87198b9e6e9cd4296561526771c0b4228b9bc5ab
kerlosrady <84878661+kerlosrady@users.noreply.github.com>
2021-11-01 08:34:57 +0200

    Update mv_r.py

--- moveit_commander/script/mv_r.py
+++ moveit_commander/script/mv_r.py
@@ -93,7 +93,7 @@ class MoveGroupPythonInterfaceTutorial(object):
       ## Now, we call the planner to compute the plan and execute it.
       plan = move_group_rarm.go(wait=True)
       global pub5
-      pub5.publish(4.0)
+      #pub5.publish(4.0)
 
       
     if dd==1:
@@ -238,6 +238,7 @@ class MoveGroupPythonInterfaceTutorial(object):
         pub3.publish("released")
         rospy.sleep(1)
         global pub5
+  
         pub5.publish(8)
 
   def rgrip_pose_goal(self,x,y):
