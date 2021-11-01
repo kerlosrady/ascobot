db990975cfc5688f803f1d393b16ecf9039d7f64
hendaafia1 <hend.aafia@gmail.com>
2021-11-01 02:48:41 +0200

    hend

--- look_hand/scripts/mission_planner.py
+++ look_hand/scripts/mission_planner.py
@@ -161,7 +161,7 @@ class mission_planner():
 						#arm 1
 						apose_goal1 = np.ones(4)
 						apose_goal1[0]=self.finalPoints.poses[0].position.x-0.27
-						apose_goal1[1]=self.finalPoints.poses[0].position.y + 0.035
+						apose_goal1[1]=self.finalPoints.poses[0].position.y + 0.034
 						apose_goal1[2]=self.finalPoints.poses[0].position.z +0.07
 						
 						apose_goal1[3]=1
@@ -173,7 +173,7 @@ class mission_planner():
 						#arm 2
 						apose_goal2 = np.ones(7)
 						apose_goal2[0]=self.finalPoints.poses[1].position.x-0.27
-						apose_goal2[1]=self.finalPoints.poses[1].position.y + 0.035
+						apose_goal2[1]=self.finalPoints.poses[1].position.y + 0.034
 						apose_goal2[2]=self.finalPoints.poses[1].position.z +0.07
 						
 						apose_goal2[3]=1
